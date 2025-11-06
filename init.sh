2843af5887bc89931c19e937409327fa8f083d72
ntw1 <ntw1@mailo.com>
2025-11-06 21:03:07 +0100

    Added cli tool setup

--- init.sh
+++ init.sh
@@ -1,77 +1,67 @@
 #!/bin/bash
 
+set -euo pipefail
+trap 'echo "Error on line $LINENO"; exit 1' ERR
+
 function print_green {
   echo "$(tput setaf 2)$1$(tput sgr0)"
 }
 
+function error_exit {
+    echo "Error: $1" >&2
+    exit 1
+}
+
 packages_cli=(
-'git'
-'stow'
-'neovim'
-'fzf'
 'bat'
 'eza'
-'zoxide'
-'fish'
-'trash-cli'
-'ttf-cascaida-mono-nerd'
-'starship'
 'fastfetch'
+'fish'
+'fzf'
 'git'
+'neovim'
+'starship'
+'stow'
+'tmux'
+'trash-cli'
+'ttf-cascadia-mono-nerd'
+'zoxide'
 )
 
-# print_green "Updating installed packages..."
-#update base packages
-# pacman -Syu
 
 print_green "Installing cli packages..."
-# echo "$(tput setaf 1)Installing cli packages...$(tput sgr0 0)"
 echo ${packages_cli[@]}
-# echo $packages_cli
+sudo pacman -S --noconfirm ${packages_cli[@]}
+# if [ $retVal -ne 0 ]; then
+#     error_exit "Error installing cli packages"
+#     exit 1
+# fi
 
-#install cli related packages
-# pacman -S $(cat packages.txt) --noconfirm
+# Tmux setup
+print_green "Clone tmux plugin manager..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# if [ $retVal -ne 0 ]; then
+#     error_exit "Error cloning tmux plugin manager"
+#     exit 1
+# fi
 
-# pacman -S git
-# pacman -S stow
-# pacman -S neovim
-# pacman -S fzf
-# pacman -S bat
-# pacman -S eza
-# pacman -S zoxide
-# pacman -S fish
-# pacman -S trash-cli
-# pacman -S ttf-cascaida-mono-nerd
-# pacman -S starship
-# pacman -S fastfetch
-# pacman -S git-delta
-#
-# update mirror list via reflector :
+# TODO make branch for laptop that installs kanata and configures it
 
-# reflector --latest 5 --country France,Germany --sort rate --save /etc/pacman.d/mirrorlist
+# Bat setup
+bat cache --build
+# if [ $retVal -ne 0 ]; then
+#     error_exit "Error cloning tmux plugin manager"
+#     exit 1
+# fi
 
-# git
-# stow
-# neovim
-# fzf
-# curl
-# bat
-# eza
-# zoxide
-# fish
-# trash cli : pacman -S trash-cli
-# delta : pacman -S git-delta
-# CaskaydiaMono Nerd Font : pacman -S ttf-cascaida-mono-nerd
-# starship
-# fastfetch
+# Set fish as default shell
+print_green "Seting fish as default shell"
+sudo chsh -s /usr/bin/fish $USER
 
-## Tmux setup
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+print_green "Cloning dotfiles..."
+git clone --recurse-submodules git@github.com:ntw1vnl/dotfiles.git ~/dotfiles
 
-## TODO make branch for laptop that installs kanata and configures it
+print_green "Stowing dotfiles..."
+cd ~/dotfiles
+stow --dotfiles .
 
-## Download bat theme an reinit bat cache
-
-# mkdir -p "$(bat --config-dir)/themes"
-# curl -Lo https://raw.githubusercontent.com/folke/tokyonight.nvim/refs/heads/main/extras/sublime/tokyonight_night.tmTheme "$(bat --config-dir)/themes/tokyonight_night.tmTheme"
-# bat cache --build
