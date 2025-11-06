f04b55554392fa0df726844164ffa75bca1c22b2
ntw1 <ntw1@mailo.com>
2025-11-06 22:44:20 +0100

    Removed comments

--- init.sh
+++ init.sh
@@ -39,29 +39,16 @@ packages_cli=(
 )
 
 print_green "Installing cli packages..."
-echo ${packages_cli[@]}
 sudo pacman -S --noconfirm ${packages_cli[@]}
-# if [ $retVal -ne 0 ]; then
-#     error_exit "Error installing cli packages"
-#     exit 1
-# fi
 
 # Tmux setup
 print_green "Clone tmux plugin manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# if [ $retVal -ne 0 ]; then
-#     error_exit "Error cloning tmux plugin manager"
-#     exit 1
-# fi
 
 # TODO make branch for laptop that installs kanata and configures it
 
 # Bat setup
 bat cache --build
-# if [ $retVal -ne 0 ]; then
-#     error_exit "Error cloning tmux plugin manager"
-#     exit 1
-# fi
 
 # Set fish as default shell
 print_green "Seting fish as default shell"
